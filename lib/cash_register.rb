feef71f0032998d1a9310b97f942c45ea0b7189b
Andy Maskiell <maskiella@gmail.com>
2017-12-30 21:04:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,6 +10,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
+    @item_price = price
     @total += (price * quantity)
     @items_quantity = quantity
     quantity.times do
