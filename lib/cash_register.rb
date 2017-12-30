72db71107cdb8d3c80e23d10dbebfcee585d2b56
Andy Maskiell <maskiella@gmail.com>
2017-12-30 20:17:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,4 +5,16 @@ class CashRegister
     @discount = discount
     @total = 0
   end
+
+  def add_item
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
 end
